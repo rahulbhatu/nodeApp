333d4984ec3413fd7e62f315e1367c97da425023
Nimesh Tailor <nieshntailor@gmail.com>
2019-11-15 16:51:35 +0530

    commit code for node app 21

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Nimesh Tailor!Welcome')
+  response.send('Hello Nimesh Tailor!Welcome to CodePipeline')
 })
 
 app.listen(app.get('port'), function() {
