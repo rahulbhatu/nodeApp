032313215884cf8fdcd2f95d9eb8e1d0ea4480ea
Nimesh Tailor <nieshntailor@gmail.com>
2019-11-15 10:51:27 +0530

    Tenth commit code for node app

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Nimesh Tailor!Welcome to codePipeline Server using codeDeploy')
+  response.send('Hello Nimesh Tailor!Welcome to codePipeline Server using codeDeploy,We are happy to help you')
 })
 
 app.listen(app.get('port'), function() {
