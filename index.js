47dc30a49067a8f0f4e620bfff3d63605e76b8d0
Nimesh Tailor <nieshntailor@gmail.com>
2019-11-15 18:12:56 +0530

    commit code for node app 25

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Nimesh Tailor!welcome to Raw Engineering')
+  response.send('Hello Nimesh Tailor!welcome to Raw Engineering,Happy to help you')
 })
 
 app.listen(app.get('port'), function() {
